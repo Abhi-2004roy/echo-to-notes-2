8c49df87d078dea2136886e1d1857ec099437bc2
Abhishek Roy <abhishekroyk6201@gmail.com>
2026-01-02 12:24:41 +0530

    Re editing cors

--- Note-By-Echo-Backend/server.js
+++ Note-By-Echo-Backend/server.js
@@ -7,12 +7,11 @@ require('dotenv').config();
 const app = express();
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.options('*', cors());
 
 // --- DEBUG CHECK ---
